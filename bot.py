eece9675938d32277a29cf8fb42af2f9bb879f4d
harrypersian138 <harry.persian138@gmail.com>
2021-12-08 18:16:29 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,6 +25,6 @@ def send_welcome(message):
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-	bot.reply_to(message, message.text)
+	bot.reply_to(message, message.text+"####")
 
 bot.infinity_polling()
